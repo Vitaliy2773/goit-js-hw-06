37b5733401d8483c73253c3ab7371ae837ab8cf8
Vitaliy2773 <vitalii64773@gmail.com>
2023-08-05 16:06:08 -1100

    change

--- js/task-06.js
+++ js/task-06.js
@@ -1,23 +1,19 @@
-const index = document.querySelector(".index.html")
-console.log(index);
-
-const input = document.querySelector("#validation-input")
-console.log(input);
-
-const lengthInput = document.querySelector('[data-length ="6"]')
-console.log(lengthInput.dataset.length);
 
 
-
-const handleInputFunction = (e) => {
-    if (input.textContent.length <= lengthInput.dataset.length) {
-        input.style.border-color(".valid")
+const input = document.querySelector("#validation-input").onblur = function () {
+    console.log(this.value.length);
+    if (this.getAttribute('data-length') >= this.value.length) {
+        this.classList.remove('invalid'),
+            this.classList.add('valid')
     }
 
-    else if (input.textContent.length > lengthInput.dataset.length) {
-       input.classList.add(".invalid")
+    else {
+        this.classList.remove('valid'),
+            this.classList.add('invalid')
     }
-   
 }
 
-input.addEventListener("blur", handleInputFunction)
+
+
+
+// input.addEventListener("blur", onblur())
---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#font-size-control')
+console.log(input);
+
+const text = document.querySelector('#text')
+console.log(text);
+
+const changeInput = input.addEventListener("input", (event) => {
+    if 
+})
