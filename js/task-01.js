e8f8da6bc25c311495c3138ebd919114a424e9ee
Vitaliy2773 <vitalii64773@gmail.com>
2023-08-12 09:23:31 -1100

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,19 @@
 
 const numberOfItem = document.querySelectorAll('.item');
-console.log(numberOfItem);
 
-numberOfItem.forEach(item => {
-    console.log(`Number of categories: ${item}`)
+
+const numberOfItemsall = numberOfItem.length
+console.log(`Number of categories: ${numberOfItemsall} `);
+
+
+numberOfItem.forEach(element => {
+    let title = element.querySelector("h2").textContent;
+
+    let item = element.querySelectorAll("li")
+    let itemElements = item.length
+    console.log(`Category:${title}`);
+    console.log(`Elements:${itemElements}`);
 })
 
 
-const categories = document.querySelector('#categories')
-console.log(categories);
-// const titleOfItem = document.querySelectorAll('h2');
-// console.log(titleOfItem)
-
-console.log(categories.textContent);
-
-
-
-
-// itemMenu.prepend(titleOfItem)
 
